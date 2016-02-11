6d58efaf54f8ecd060fca2f33a223cc92cc0551a
unknown <you@example.com>
2016-02-11 14:00:23 -0500

    added node port

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
+var app = express();
+
+var PORT = process.env.NODE_ENV || 8060;
+app.engine('handlebars', expressHandlebars({defaultLayout: 'burgers'}));
+app.set('view engine', 'handlebars');
+
+
+app.listen(PORT, function() {
+  console.log('Listening on %s', PORT);
+});
