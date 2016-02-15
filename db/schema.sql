f8f666a01e7e49457f506b754883f847a9adc8de
unknown <you@example.com>
2016-02-15 15:55:03 -0500

    updated connection.js file

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,11 @@
 CREATE DATABASE burgers_db;
-CREATE TABLE burgers (b_id INT NOT NULL AUTO_INCREMENT, burger_name VARCHAR(255), devoured BOOLEAN, date_d TIMESTAMP);
+
+USE burgers_db;
+
+CREATE TABLE burgers 
+(
+  b_id INT NOT NULL AUTO_INCREMENT, 
+  burger_name VARCHAR(255), 
+  devoured BOOLEAN, 
+  date_d TIMESTAMP
+);
